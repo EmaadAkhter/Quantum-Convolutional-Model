c9466b8d2ca1f69175e57214a19148684d264adb
Emaad_Ansari <67855951+EmaadAkhter@users.noreply.github.com>
2025-08-07 13:16:52 +0530

    Delete Code directory

--- Code/plot_utils.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-from PIL import Image
-
-def visualize_prediction(fname, res):
-    img = Image.open(fname).convert('L')
-    plt.imshow(img, cmap='gray')
-    plt.title(f"Predicted: {res['digit']} (Confidence: {res['confidence']:.2f})")
-    plt.axis('off')
-    plt.savefig(fname.replace('.jpg', f'{fname}_pred.png'), bbox_inches='tight')
